5a2bda911c03c529b1297a2cf8ceedd13e5a20d2
forge 1.12.2 <150913393+minecraftforge-1-12-2@users.noreply.github.com>
2024-08-17 04:36:40 +0500

    a

--- game/main.lua
+++ game/main.lua
@@ -4,16 +4,16 @@ joystick = joysticks[1]
 keyboard = {}
 gamepad = {}
 mouse = {}
---function love.keypressed(key)
---	keyboard.key = key
---end
---function love.mousepressed(x, y, button)
---	if playerexists then
---		if button == 1 and shoottimer <= 0 then
---			shoot(player.x + 24, player.y + 12, angle)
---		end
---	end
---end
+function love.keypressed(key)
+	keyboard.key = key
+end
+function love.mousepressed(x, y, button)
+	if playerexists then
+		if button == 1 and shoottimer <= 0 then
+			shoot(player.x + 24, player.y + 12, angle)
+		end
+	end
+end
 function love.gamepadpressed(joystick, button)
 	gamepad.button = button
 	if playerexists then
@@ -32,11 +32,11 @@ player_exists = false
 level = "mainmenu"
 walls = {}
 mainmenu = require "src.mainmenu"
---quest1intro = require "src.events.quest1intro"
+quest1intro = require "src.events.quest1intro"
 goxiosunderground = require "src.worlds.goxiosunderground"
 function love.load()
 	mainmenu.load()
-	--quest1intro.load()
+	quest1intro.load()
 	goxiosunderground.load()
 	Object = require "classic"
 	bump = require "bump"
@@ -49,33 +49,32 @@ function love.load()
 	loadPlayerImages()
 end
 function love.update(dt)
-	--input.start = keyboard.key == "return"
-	--input.left = love.keyboard.isDown("a")
-	--input.right = love.keyboard.isDown("d")
-	--input.up = love.keyboard.isDown("w")
-	--input.down = love.keyboard.isDown("s")
-	--input.jump = love.keyboard.isDown("space")
-	--mouse_x, mouse_y = love.mouse.getPosition()
+	input.start = keyboard.key == "return"
+	input.left = love.keyboard.isDown("a")
+	input.right = love.keyboard.isDown("d")
+	input.up = love.keyboard.isDown("w")
+	input.down = love.keyboard.isDown("s")
+	input.jump = love.keyboard.isDown("space")
+	mouse_x, mouse_y = love.mouse.getPosition()
 	if joystick then
-		input.start = (gamepad.button == "start") --or keyboard.key == "return"
-		input.left = (joystick:isGamepadDown("dpleft")) --or love.keyboard.isDown("a")
-		input.right = (joystick:isGamepadDown("dpright")) --or love.keyboard.isDown("d")
-		input.up = (joystick:isGamepadDown("dpup")) --or love.keyboard.isDown("w")
-		input.down = (joystick:isGamepadDown("dpdown")) --or love.keyboard.isDown("s")
-		input.jump = (joystick:isGamepadDown("a")) --or love.keyboard.isDown("space")
+		input.start = (gamepad.button == "start") or keyboard.key == "return"
+		input.left = (joystick:isGamepadDown("dpleft")) or love.keyboard.isDown("a")
+		input.right = (joystick:isGamepadDown("dpright")) or love.keyboard.isDown("d")
+		input.up = (joystick:isGamepadDown("dpup")) or love.keyboard.isDown("w")
+		input.down = (joystick:isGamepadDown("dpdown")) or love.keyboard.isDown("s")
+		input.jump = (joystick:isGamepadDown("a")) or love.keyboard.isDown("space")
 		mouse_x, mouse_y = (joystick:getAxis(3)) * 100  + 480, (joystick:getAxis(4)) * 100 + 270
 	end
 	if input.jump then
 		pJump()
 	end
 	
-	--print(mouse_x, mouse_y)
 	angle = math.atan2(mouse_y - 270 + 24, mouse_x - 480 + 24 )
 	if input.start then
 		if level == "mainmenu" then
 			mainmenu.music:stop()
 			mainmenu = false
-			level = "goxiosunderground"
+			level = "quest1intro"
 		end
 	end
 	--goxiosunderground
@@ -94,7 +93,7 @@ function love.draw()
 		mainmenu.draw()
 	end
 	if level == "quest1intro" then
-		--quest1intro.play()
+		quest1intro.play()
 	end
 	if playerexists then
 		love.graphics.translate(-player.x + 480 - 24, -player.y + 270 - 24)
